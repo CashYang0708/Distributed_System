91fa5ed42be14a13e502fe6db7591f67d0a41584
CashYang0708 <CashYang16@gmail.com>
2023-05-01 18:57:10 +0800

    add rest

---
+++ rest/restful-server.js
@@ -0,0 +1,46 @@
+const server = require('fastify')();
+
+let john = {
+    name: "john",
+    age: 18,
+    attack: 100,
+    defense: 100
+};
+
+let tom = {
+    name: "tom",
+    age: 19,
+    attack: 105,
+    defense: 90
+};
+
+let hogRiders = [john, tom];
+
+server.get('/hogRider', function (req, res) {
+    return hogRiders;
+});
+
+server.get('/hogRider/:name', function (req, res) {
+    // 請依Lab說明寫作
+    let result = hogRiders.find(element => element.name === req.params.name);
+    if(result){
+        return result;
+    }
+    return {'error':'not found'};
+});
+
+server.post('/hogRider', function (req, res) {
+    // 請依Lab說明寫作
+    hogRiders.push(req.body);
+    return {count: hogRiders.length};
+});
+
+server.put('/hogRider/:name', function (req, res) {
+    // 請依Lab說明寫作
+    let index = hogRiders.findIndex(element => element.name === req.params.name);
+    hogRiders[index]=req.body;
+    return hogRiders[index];
+});
+
+
+server.listen(3000, "localhost");
